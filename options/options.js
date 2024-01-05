c0b2d547a876f57aa936325a196a8502cdb3a58e
Crowdbotics <engineering@crowdbotics.com>
2024-01-05 16:05:53 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-poetry-57",
+  url: "https://winter-poetry-57.botics.co",
+  api: "https://winter-poetry-57.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
